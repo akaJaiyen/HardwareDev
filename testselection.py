8c30329a66362e19abcacbb38c0da3d408e12dbe
akaJaiyen <kirana.ra@ku.th>
2025-12-03 16:38:27 +0700

    Add files via upload

---
+++ testselection.py
@@ -0,0 +1,16 @@
+import pygame, sys
+from button import Button
+import datetime
+import os
+import random
+import threading
+pygame.init()
+
+if userInput[pygame.K_RIGHT]:
+    A += 320
+    print(A)
+    
+if userInput[pygame.K_LEFT]:
+    A -= 320
+    print(A)
+    
